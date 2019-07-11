42d1bff63eacebb5b7d845777ea42817289edd8d
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-11 14:42:36 +0200

    Change app directory
    Change app into a class component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import App from "./Components/App";
+import App from "./Components/App/App";
 import store from "./store/store";
 
 ReactDOM.render(
