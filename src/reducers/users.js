4d7b23e62de3bd2cd94bb293ebc9f63ffaf5bdfc
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-11 15:39:06 +0200

    Create case ADD_USERS in users reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,10 @@
-const users = (state = {}, action) => {
-  switch (action.type) {
-    case "CHANGE_IT":
+import { ADD_USERS } from "../actions/actionTypes";
 
+const users = (state = {}, action) => {
+  console.log(action.type);
+  switch (action.type) {
+    case ADD_USERS:
+      return { ...action.payload };
     default:
       return state;
   }
