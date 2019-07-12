0e6788f18ff80397dc4c18fafd0fdf41dd473d49
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-12 13:26:50 +0200

    Create conditional rendering in App.js
    Render Loader until users fetch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import Page from "../Page/Page";
+import Loader from "../Loader/Loader";
 import fetchUsers from "../../actions/fetchUsers";
 
 class App extends React.Component {
   componentDidMount() {
     this.props.fetchUsers();
   }
-
   render() {
-    return <div>APP</div>;
+    const users = this.props.users;
+    return users ? <Page /> : <Loader />;
   }
 }
 const mapStateToProps = state => ({ users: state.users });
