cd156b2f739579423a7b7203c44f975d90159a81
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-12 13:57:01 +0200

    Create user items in UserListPage
    get users from store

--- src/Components/UsersListPage/UsersListPage.js
+++ src/Components/UsersListPage/UsersListPage.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const UserListPage = () => {
-  return <ul />;
+const UserListPage = props => {
+  const users = props.users;
+  const usersArray = Object.values(users);
+  const userItems = usersArray.map(user => {
+    const { id, name, email, phone, website } = user;
+    return (
+      <li key={id}>
+        <h1>{name}</h1>
+        <h3>{email}</h3>
+        <h3>{phone}</h3>
+        <h3>{website}</h3>
+        <button name={id}>Details</button>
+      </li>
+    );
+  });
+  return <ul> {userItems}</ul>;
 };
 
 const mapStateToProps = state => ({ users: state.users });
