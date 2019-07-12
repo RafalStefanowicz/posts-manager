6508d49c729b7444d8d507a081723b8332ac4e69
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-12 17:02:36 +0200

    Remove console.log

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,6 @@
 import { ADD_USERS } from "../actions/actionTypes";
 
 const users = (state = {}, action) => {
-  console.log(action.type);
   switch (action.type) {
     case ADD_USERS:
       return { ...action.payload };
