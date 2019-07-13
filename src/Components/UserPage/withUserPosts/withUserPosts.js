23532b3a9b55dd82b3918c71e5f8bc1b357e6be6
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-13 10:12:29 +0200

    Add check userId conditions to withUserPosts.js
    pass to rendered component only posts of searched user

--- src/Components/UserPage/withUserPosts/withUserPosts.js
+++ src/Components/UserPage/withUserPosts/withUserPosts.js
@@ -2,26 +2,35 @@ import React from "react";
 import { connect } from "react-redux";
 
 import fetchUserPosts from "../../../actions/fetchUserPosts";
+import InfoPage from "../../InfoPage/InfoPage";
+import Loader from "../../Loader/Loader";
 
 const withUserPosts = Component => {
   class WithUserPosts extends React.Component {
     componentDidMount() {
-      this.props.fetchUserPosts(this.props.user.id);
+      //only fetch if there is userId of existing user
+      if (this.props.user) {
+        this.props.fetchUserPosts(this.props.user.id);
+      }
     }
     render() {
-      return (
-        <Component
-          user={this.props.user}
-          posts={this.props.posts}
-          {...this.props}
-        />
+      const { user, posts } = this.props;
+      //prevent if the user manually entered url of not existing id
+      if (user === null) {
+        return <InfoPage>Nie ma takiego użytkownika</InfoPage>;
+      }
+      return posts ? (
+        <Component user={user} posts={posts} {...this.props} />
+      ) : (
+        <Loader />
       );
     }
   }
   const mapStateToProps = (state, props) => {
     const userId = props.match.params.userId;
+    const posts = state.posts[userId];
     const user = state.users[userId] || null;
-    return { user, posts: state.posts };
+    return { user, posts };
   };
   const mapDispatchToProps = { fetchUserPosts };
   return connect(
