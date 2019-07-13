82015bbf7b1cc84f7313f21242db262b852c4a58
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-13 11:09:54 +0200

    Create conditional rendering in UserPage.js
    render PostDetails or Posts depend on params

--- src/Components/UserPage/UserPage.js
+++ src/Components/UserPage/UserPage.js
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 
 import withUser from "./withUserPosts/withUserPosts";
 import UserNav from "./UserNav/UserNav";
+import Posts from "./Posts/Posts";
+import PostDetails from "./PostDetails/PostDetails";
 class UserPage extends Component {
-  userId = this.props.match.params.userId;
-  postId = this.props.match.params.postId;
-
   componentDidMount() {}
+
   componentWillUnmount() {}
 
   render() {
-    const { user } = this.props;
+    const userId = this.props.match.params.userId;
+    const postId = this.props.match.params.postId;
+    const searchedPost = this.props.posts.find(post => post.id == postId);
+    const { user, posts } = this.props;
     return (
-      <UserNav userId={this.userId} postId={this.postId} userName={user.name} />
+      <>
+        <UserNav userId={userId} postId={postId} userName={user.name} />
+        {postId ? <PostDetails post={searchedPost} /> : <Posts posts={posts} />}
+      </>
     );
   }
 }
