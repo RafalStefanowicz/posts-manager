7de5e4dcec72c7434587d0e3d03e474ddd5f45d3
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-13 14:02:51 +0200

    Create fetchPostComments.js action
    get comments from api

---
+++ src/actions/fetchPostComments.js
@@ -0,0 +1,10 @@
+import { FETCH_POST_COMMENTS } from "./actionTypes";
+import usersApi from "../apis/usersApi";
+
+const fetchPostComments = postId => async dispatch => {
+  const response = await usersApi.get(`/comments?postId=${postId}`);
+  const comments = response.data;
+  dispatch({ type: FETCH_POST_COMMENTS, payload: { [postId]: comments } });
+};
+
+export default fetchPostComments;
