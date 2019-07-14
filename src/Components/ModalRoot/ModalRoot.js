4c8a70db5730cd231c2db36db1e84747edc89365
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-14 16:06:59 +0200

    Create ModalRoot.js and conditional modal render

---
+++ src/Components/ModalRoot/ModalRoot.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import {
+  ADD_POST_MODAL,
+  DELETE_POST_MODAL,
+  ADD_COMMENT_MODAL
+} from "../../constants/modalTypes";
+import AddPostModal from "../AddPostModal/AddPostModal";
+
+const ModalRoot = props => {
+  const modalType = props.modal.modalType;
+  const modalProps = props.modal.modalProps;
+  switch (modalType) {
+    case ADD_POST_MODAL:
+      return <AddPostModal userId={modalProps.userId} />;
+    // case DELETE_POST_MODAL:
+    //   return <DeletePostModal post={modalProps} />;
+    // case ADD_COMMENT_MODAL:
+    //   return <AddCommentModal post={modalProps} />;
+    default:
+      return null;
+  }
+};
+const mapStateToProps = state => ({ modal: state.modal });
+
+export default connect(mapStateToProps)(ModalRoot);
