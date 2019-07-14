39d6f9a60f65fa332357aaa3b097039e46fec6c1
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-14 09:33:47 +0200

    Add ModalRoot.js to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
+import ModalRoot from "./Components/ModalRoot/ModalRoot";
 import App from "./Components/App/App";
 import store from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
+    <ModalRoot />
     <App />
   </Provider>,
 
