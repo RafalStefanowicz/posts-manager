a278facceef07186d77dafd1a77fbb041fbc314a
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-14 09:40:43 +0200

    Add showModal action to AddPostBtn

--- src/Components/AddPostBtn/AddPostBtn.js
+++ src/Components/AddPostBtn/AddPostBtn.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { ADD_POST_MODAL } from "../../constants/modalTypes";
+import showModal from "../../actions/showModal";
 
 const AddPostBtn = props => {
-  return <button>Dodaj Post</button>;
+  const handleAddPost = () => {
+    props.showModal(ADD_POST_MODAL, { userId: props.userId });
+  };
+  return <button onClick={handleAddPost}>Add Post</button>;
 };
-export default AddPostBtn;
+
+AddPostBtn.propTypes = { userId: PropTypes.number.isRequired };
+
+const mapDispatchToProps = { showModal };
+export default connect(
+  null,
+  mapDispatchToProps
+)(AddPostBtn);
