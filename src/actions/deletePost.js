66a8f748dc4909d4cc4562467ffa3db0a5388dad
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-15 16:11:04 +0200

    Add history to deletePost.js

--- src/actions/deletePost.js
+++ src/actions/deletePost.js
@@ -1,10 +1,14 @@
 import { DELETE_POST } from "./actionTypes";
 import usersApi from "../apis/usersApi";
 import hideModal from "./hideModal";
+import history from "../history/history";
+import { USER } from "../constants/routes";
 
 const deletePost = postId => async dispatch => {
   await usersApi.delete(`/posts/${postId}`);
   dispatch({ type: DELETE_POST, payload: postId });
   dispatch(hideModal());
+  console.log("get user from store and push");
+  // history.push(`/user/1`);
 };
 export default deletePost;
