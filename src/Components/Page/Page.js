b7d31509b0dfa66429b9c1db1214a15d9ce0ce29
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-15 16:04:53 +0200

    Pass history to Router
    instead of using BrowserRouter
    
    action deletePost need history to push url when resolved

--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
 
 import { USERSLIST, USER } from "../../constants/routes";
 import UserListPage from "../UsersListPage/UsersListPage";
 import UserPage from "../UserPage/UserPage";
+import history from "../../history/history";
 const Page = () => {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Switch>
         <Route path={USER} component={UserPage} />
         <Route path={USERSLIST} component={UserListPage} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 };
 export default Page;
