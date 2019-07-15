299f4e6cd30d3c3f18137256c3a25a4be4a3dbcb
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-15 11:17:30 +0200

    Remove unused redux and prop in AddPostModal.js

--- src/Components/AddPostModal/AddPostModal.js
+++ src/Components/AddPostModal/AddPostModal.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import addPost from "../../actions/addPost";
 import AddPostForm from "../AddPostForm/AddPostForm";
 import Modal from "../Modal/Modal";
 
 const AddPostModal = props => {
   const { userId } = props;
-  return <Modal heading="Add post">{<AddPostForm userId={userId} />}</Modal>;
+  return <Modal>{<AddPostForm userId={userId} />}</Modal>;
 };
 
 AddPostModal.propTypes = { userId: PropTypes.number.isRequired };
 
-const mapDispatchToPropst = { addPost };
-export default connect(
-  null,
-  mapDispatchToPropst
-)(AddPostModal);
+export default AddPostModal;
