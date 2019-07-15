28391f83e6f87ade39e39b9df2a3e27c5c67d0a2
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-15 08:41:51 +0200

    Add try..catch to addPost.js

--- src/actions/addPost.js
+++ src/actions/addPost.js
@@ -6,16 +6,20 @@ import UniqueId from "../helpers/UniqueId";
 const uniqueId = new UniqueId(100);
 
 const addPost = (userId, post) => async dispatch => {
-  const response = await usersApi.post(`/posts/`, {
-    userId: userId,
-    title: post.title,
-    body: post.body
-  });
-  const responsePost = response.data;
-  // reassign post.id , because api always returns 101
-  responsePost.id = uniqueId.getUniqueId();
-  dispatch({ type: ADD_POST, payload: responsePost });
-  dispatch(hideModal());
+  try {
+    const response = await usersApi.post(`/posts/`, {
+      userId: userId,
+      title: post.title,
+      body: post.body
+    });
+    const responsePost = response.data;
+    // reassign post.id , because api always returns 101
+    responsePost.id = uniqueId.getUniqueId();
+    dispatch({ type: ADD_POST, payload: responsePost });
+    dispatch(hideModal());
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default addPost;
