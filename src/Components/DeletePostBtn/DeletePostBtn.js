cc2df2c8bd4ed2d0178f926ece75b582fe1fb40a
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-15 12:09:00 +0200

    Move logic to DeletePostBtn

--- src/Components/DeletePostBtn/DeletePostBtn.js
+++ src/Components/DeletePostBtn/DeletePostBtn.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import showModal from "../../actions/showModal";
+import { DELETE_POST_MODAL } from "../../constants/modalTypes";
 
 const DeletePostBtn = props => {
-  return <button>Usuń Post</button>;
+  const handleDeleteClick = () => {
+    props.showModal(DELETE_POST_MODAL, { postId: props.postId });
+  };
+
+  return <button onClick={handleDeleteClick}>Usuń Post</button>;
 };
-export default DeletePostBtn;
+
+const mapDispatchToProps = { showModal };
+
+DeletePostBtn.propTypes = {
+  postId: PropTypes.number.isRequired
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(DeletePostBtn);
--- src/Components/UserPage/Posts/Posts.js
+++ src/Components/UserPage/Posts/Posts.js
@@ -1,26 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-import showModal from "../../../actions/showModal";
-import { DELETE_POST_MODAL } from "../../../constants/modalTypes";
+import DeletePostBtn from "../../DeletePostBtn/DeletePostBtn";
 const Posts = props => {
-  const handleDeleteClick = post => {
-    props.showModal(DELETE_POST_MODAL, { post: post });
-  };
-
   const postItems = props.posts.map(post => {
     const { userId, id: postId, title } = post;
     return (
       <li key={postId}>
-        <button
-          onClick={() => {
-            handleDeleteClick(post);
-          }}
-        >
-          Usun
-        </button>
+        <DeletePostBtn postId={postId} />
         <Link to={`/user/${userId}/${postId}`}>
           <h1>{title}</h1>
           <span>ikonka</span>
@@ -34,9 +22,5 @@ const Posts = props => {
 Posts.propTypes = {
   posts: PropTypes.array.isRequired
 };
-const mapDispatchToProps = { showModal };
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(Posts);
+export default Posts;
