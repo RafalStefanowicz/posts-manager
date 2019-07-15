256500c312762d05e32a03b4ffbb1203b56f668e
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-15 14:24:19 +0200

    Dispatch hideModal action in DeletePostModal.js

--- src/Components/DeletePostModal/DeletePostModal.js
+++ src/Components/DeletePostModal/DeletePostModal.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import Modal from "../Modal/Modal";
+import hideModal from "../../actions/hideModal";
 import deletePost from "../../actions/deletePost";
 
 const DeletePostModal = props => {
@@ -13,14 +14,15 @@ const DeletePostModal = props => {
   return (
     <Modal>
       <h1>{`Do u want to delete post ${postId} ?`}</h1>
-      <button onClick={handleDeleteClick}>Tak</button>
+      <button onClick={hideModal}>Cancel</button>
+      <button onClick={handleDeleteClick}>Delete</button>
     </Modal>
   );
 };
 
 DeletePostModal.propTypes = { postId: PropTypes.number.isRequired };
 
-const mapDispatchToProps = { deletePost };
+const mapDispatchToProps = { deletePost, hideModal };
 
 export default connect(
   null,
