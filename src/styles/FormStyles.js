edd9040dede1339c483e28de8bddc47e16620588
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:25:44 +0200

    Create FormStyles.js

---
+++ src/styles/FormStyles.js
@@ -0,0 +1,67 @@
+import styled, { css } from "styled-components";
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledLabel = styled.label`
+  display: inline-block;
+  width: 25%;
+  font-size: 24px;
+  line-height: 1px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const StyledInput = styled.input`
+  width: 75%;
+  height: 30px;
+  border: 2px solid black;
+  font-size: 20px;
+`;
+
+export const StyledTextArea = styled.textarea`
+  width: 75%;
+  height: 80px;
+  border: 2px solid black;
+  font-size: 20px;
+`;
+
+export const ButtonsWrapper = styled.div`
+  text-align: ${props => (props.right ? "right" : "center")};
+`;
+
+export const StyledButton = styled.button`
+  width: 25%;
+  padding: 10px;
+  margin: 10px;
+  border: 3px solid black;
+  font-size: 20px;
+  font-weight: 700;
+  ${props =>
+    props.disabled
+      ? css`
+          border: 3px solid gray;
+        `
+      : css`
+          :hover {
+            background-color: #0b5394;
+            color: white;
+          }
+        `}
+`;
+
+export const StyledP = styled.p`
+  color: red;
+  text-align: center;
+`;
+
+export const StyledForm = styled.form`
+  width: 85%;
+  margin: 0 auto;
+`;
+
+export const ErrorWrapper = styled.div`
+  height: 30px;
+`;
