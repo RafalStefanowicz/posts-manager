1613838677396a7da4ddd7728f5b68c49193b069
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 22:24:52 +0200

    Change margins and paddings in UserListPageStyle.js

--- src/Components/UsersListPage/UsersListPageStyle.js
+++ src/Components/UsersListPage/UsersListPageStyle.js
@@ -10,22 +10,22 @@ export const StyledItem = styled.li`
   width: 30%;
   min-width: 300px;
   margin: 20px;
-  padding: 30px;
+  padding: 30px 40px;
   border: 2px solid black;
 `;
 
 export const StyledName = styled.h1`
-  margin: 10px;
+  margin: 10px 0;
 `;
 
 export const StyledDetails = styled.h3`
-  margin: 8px;
+  margin: 8px 0;
 `;
 
 export const StyledButton = styled.button`
   width: 100%;
   height: 70px;
-  margin: 8px;
+  margin: 8px 0;
   border: 1px solid black;
   font-size: 30px;
 `;
