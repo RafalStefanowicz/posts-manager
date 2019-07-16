df14179e9788719fd8d55b1020ff286e3861287f
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:48:06 +0200

    Add styles to Loader.js

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -2,14 +2,14 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
+import { StyledHeading, StyledLabel } from "./LoaderStyles";
+
 const Loader = () => {
   return (
-    <div>
-      <h1>
-        <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
-        Logowanie..
-      </h1>
-    </div>
+    <StyledHeading>
+      <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
+      <StyledLabel>Logowanie..</StyledLabel>
+    </StyledHeading>
   );
 };
 export default Loader;
