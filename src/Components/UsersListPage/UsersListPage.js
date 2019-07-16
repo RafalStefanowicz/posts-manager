cd0958f4ba81368f3d14ba106eee8d801c901510
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:42:34 +0200

    Create styles to UsersListPage.js

--- src/Components/UsersListPage/UsersListPage.js
+++ src/Components/UsersListPage/UsersListPage.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { Wrapper } from "../../styles/common";
+import {
+  StyledList,
+  StyledItem,
+  StyledName,
+  StyledDetails,
+  StyledButton
+} from "./UsersListPageStyle";
+
 const UserListPage = props => {
   const users = props.users;
   const usersArray = Object.values(users);
@@ -13,19 +22,23 @@ const UserListPage = props => {
   const userItems = usersArray.map(user => {
     const { id, name, email, phone, website } = user;
     return (
-      <li key={id}>
-        <h1>{name}</h1>
-        <h3>{email}</h3>
-        <h3>{phone}</h3>
-        <h3>{website}</h3>
-        <button onClick={onClick} id={id}>
+      <StyledItem key={id}>
+        <StyledName>{name}</StyledName>
+        <StyledDetails>{email}</StyledDetails>
+        <StyledDetails>{phone}</StyledDetails>
+        <StyledDetails>{website}</StyledDetails>
+        <StyledButton onClick={onClick} id={id}>
           Details
-        </button>
-      </li>
+        </StyledButton>
+      </StyledItem>
     );
   });
 
-  return <ul> {userItems}</ul>;
+  return (
+    <Wrapper>
+      <StyledList> {userItems}</StyledList>{" "}
+    </Wrapper>
+  );
 };
 
 const mapStateToProps = state => ({ users: state.users });
---
+++ src/Components/UsersListPage/UsersListPageStyle.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const StyledItem = styled.li`
+  width: 30%;
+  min-width: 300px;
+  margin: 20px;
+  padding: 30px;
+  border: 2px solid black;
+`;
+
+export const StyledName = styled.h1`
+  margin: 10px;
+`;
+
+export const StyledDetails = styled.h3`
+  margin: 8px;
+`;
+
+export const StyledButton = styled.button`
+  width: 100%;
+  height: 70px;
+  margin: 8px;
+  border: 1px solid black;
+  font-size: 30px;
+`;
