44820ede882c3772960e221a50c1332cdcb728b1
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:45:51 +0200

    Add styles to AddPostBtn.js

--- src/Components/AddPostBtn/AddPostBtn.js
+++ src/Components/AddPostBtn/AddPostBtn.js
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 
 import { ADD_POST_MODAL } from "../../constants/modalTypes";
 import showModal from "../../actions/showModal";
-
+import { StyledButton } from "./AddPostBtnStyles";
 const AddPostBtn = props => {
   const handleAddPost = () => {
     props.showModal(ADD_POST_MODAL, { userId: props.userId });
   };
-  return <button onClick={handleAddPost}>Add Post</button>;
+  return <StyledButton onClick={handleAddPost}>+</StyledButton>;
 };
 
 AddPostBtn.propTypes = { userId: PropTypes.number.isRequired };
