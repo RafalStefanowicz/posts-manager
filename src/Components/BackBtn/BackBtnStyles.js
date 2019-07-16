e7756485580d4edefd8a0815d315a30dfefd8e8c
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:27:39 +0200

    Create BackBtnStyles.js

---
+++ src/Components/BackBtn/BackBtnStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  color: #0b5394;
+`;
+
+export const Label = styled.span`
+  margin: 3px;
+  font-size: 20px;
+`;
+
+export const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  font-size: 60px;
+`;
