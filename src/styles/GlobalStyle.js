3a130ca603fc38f13e9f7486b0e3d0c7cbabf953
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:23:41 +0200

    Add button to GlobalStyle.js

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,22 +1,31 @@
 import { createGlobalStyle } from "styled-components";
-import React from "react";
 const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
+
     body {
         @import url("https://fonts.googleapis.com/css?family=Lato:400,700&display=swap");
         font-family: "Lato", sans-serif;
     }
+
     a {
         text-decoration: none;
         color: black;
     }
+
+    button {
+        background-color: white;
+        border: none;
+        outline: 0;
+    }
+
     button:hover {
         cursor: pointer;
     }
+
     li {
        list-style-type: none;
     }
