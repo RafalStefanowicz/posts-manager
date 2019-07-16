c0f57c1800508788a10c342f6645f9d4638f0855
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:47:20 +0200

    Add styles and FontAwesomeIcon to BackBtn.js

--- src/Components/BackBtn/BackBtn.js
+++ src/Components/BackBtn/BackBtn.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
+import { StyledButton, StyledFontAwesomeIcon, Label } from "./BackBtnStyles";
 const BackButton = props => {
   const handleClick = () => {
     props.history.push(props.path);
   };
-  return <button onClick={handleClick}>Cofnij</button>;
+  return (
+    <StyledButton onClick={handleClick}>
+      <StyledFontAwesomeIcon icon={faArrowLeft} />
+      <Label>Back</Label>
+    </StyledButton>
+  );
 };
 export default withRouter(BackButton);
