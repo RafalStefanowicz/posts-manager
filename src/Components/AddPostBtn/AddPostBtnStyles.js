d11d3ce126cb2f5c69514f5469e635a6cdcbedd8
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-16 10:34:53 +0200

    Create styles in AddPostBtnStyles.js

---
+++ src/Components/AddPostBtn/AddPostBtnStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  color: white;
+  background-color: #0b5394;
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  font-size: 40px;
+`;
