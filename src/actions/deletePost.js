b9524386fd41257d1810a10f7cb43128390c60b3
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-07-17 11:33:05 +0200

    Dispatch action when post has been deleted

--- src/actions/deletePost.js
+++ src/actions/deletePost.js
@@ -1,13 +1,15 @@
 import { DELETE_POST } from "./actionTypes";
 import usersApi from "../apis/usersApi";
 import hideModal from "./hideModal";
+import changeIsDeletingState from "./changeIsDeletingState";
 import history from "../history/history";
 import { USER } from "../constants/routes";
 
 const deletePost = (postId, userId) => async dispatch => {
   try {
     await usersApi.delete(`/posts/${postId}`);
-    dispatch({ type: DELETE_POST, payload: postId });
+    await dispatch({ type: DELETE_POST, payload: postId });
+    dispatch(changeIsDeletingState(false));
     dispatch(hideModal());
     history.push(USER + userId);
   } catch (error) {
