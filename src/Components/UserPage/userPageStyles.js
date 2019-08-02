e6b95f796483dda05a39d16815bfdc6e0755529e
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-08-02 10:09:42 +0200

    Change position fixed only on mobiles

--- src/Components/UserPage/userPageStyles.js
+++ src/Components/UserPage/userPageStyles.js
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 
 export const StyledUserNavWrapper = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
+  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 30px;
-  background-color: white;
   @media (max-width: 768px) {
     padding: 15px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: white;
   }
 `;
 
 export const StyledUserMainWrapper = styled.main`
-  margin-top: 120px;
   @media (max-width: 768px) {
     margin-top: 90px;
   }
